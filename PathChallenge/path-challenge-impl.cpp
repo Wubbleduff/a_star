985665b566e171476107e119e49d65bff29175b1
Michael Fritz <michael.s.fritz@gmail.com>
2021-12-23 00:53:55 -0800

    Trying to get walls to work.

--- PathChallenge/path-challenge-impl.cpp
+++ PathChallenge/path-challenge-impl.cpp
@@ -34,51 +34,280 @@ static constexpr U32 kCells = kRows * kCols;
 static constexpr U32 kStartI = kStartY * kCols + kStartX;
 static constexpr U32 kEndI = kEndY * kCols + kEndX;
 
+static const double SQRT_2 = 1.4142135623730951;
+
+struct v2
+{
+    U32 x, y;
+
+    bool operator==(const v2 &other) const
+    {
+        return x == other.x && y == other.y;
+    }
+};
+
+struct Node
+{
+    v2 pos;
+    double f_score;
+};
+
+double heuristic(v2 a, v2 b)
+{
+    return (b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y);
+    // int dx = a.x - b.x;
+    // int dy = a.y - b.y;
+    // dx = dx < 0.0 ? -dx : dx;
+    // dy = dy < 0.0 ? -dy : dy;
+    // return (dx + dy) + (SQRT_2 - 2 * 1) * ((dx < dy) ? dx : dy);
+}
+
+/*
+(3, 1)
+
+node_i = 13
+chunk_i = 1
+wall_bits = 0 0 0 0 0 1 0 0 
+mask =      0 0 0 0 0 1 0 0 
+
+  0 1 2 3 4 5 6 7 8 9
+0 _ _ _ _ _ _ _ _ _ _
+1 _ _ _ X _ _ _ _ _ _
+2 _ _ _ _ _ _ _ _ _ _
+3 _ _ _ _ _ _ _ _ _ _
+4 _ _ _ _ _ _ _ _ _ _
+5 _ _ _ _ _ _ _ _ _ _
+6 _ _ _ _ _ _ _ _ _ _
+7 _ _ _ _ _ _ _ _ _ _
+*/
+bool is_wall(v2 node, const U64 *walls)
+{
+    int bit_index = node.y * kRows + node.x;
+    U64 wall_chunk_bits = walls[bit_index / 64];
+    U64 mask_pos = (63 - (bit_index % 64));
+    U64 mask = 1ULL << mask_pos;
+    return wall_chunk_bits & mask;
+}
+
+void find_neighbors(v2 node, const U64 *walls, v2 out[8], double out_distances[8], int *num_out)
+{
+    v2 tl = {node.x - 1, node.y + 1};
+    v2 t  = {node.x,     node.y + 1};
+    v2 tr = {node.x + 1, node.y + 1};
+    v2 ml = {node.x - 1, node.y};
+    v2 mr = {node.x + 1, node.y};
+    v2 bl = {node.x - 1, node.y - 1};
+    v2 b  = {node.x,     node.y - 1};
+    v2 br = {node.x + 1, node.y - 1};
+
+    if(node.x >= 1 && node.y <= kRows - 1 && !is_wall(tl, walls))
+    {
+        out_distances[*num_out] = SQRT_2;
+        out[(*num_out)++] = tl;
+    }
+    if(node.y <= kRows - 1 && !is_wall(t, walls))
+    {
+        out_distances[*num_out] = 1.0f;
+        out[(*num_out)++] = t;
+    }
+    if(node.x <= kCols - 1 && node.y <= kRows - 1 && !is_wall(tr, walls))
+    {
+        out_distances[*num_out] = SQRT_2;
+        out[(*num_out)++] = tr;
+    }
+    if(node.x >= 1 && !is_wall(ml, walls))
+    {
+        out_distances[*num_out] = 1.0f;
+        out[(*num_out)++] = ml;
+    }
+    if(node.x <= kCols - 1 && !is_wall(mr, walls))
+    {
+        out_distances[*num_out] = 1.0f;
+        out[(*num_out)++] = mr;
+    }
+    if(node.x >= 1 && node.y >= 1 && !is_wall(bl, walls))
+    {
+        out_distances[*num_out] = SQRT_2;
+        out[(*num_out)++] = bl;
+    }
+    if(node.y >= 1 && !is_wall(b, walls))
+    {
+        out_distances[*num_out] = 1.0f;
+        out[(*num_out)++] = b;
+    }
+    if(node.x <= kCols - 1 && node.y >= 1 && !is_wall(br, walls))
+    {
+        out_distances[*num_out] = SQRT_2;
+        out[(*num_out)++] = br;
+    }
+}
+
+#if 1
+
+void print_walls(const U64 *walls)
+{
+    printf("\n\n\n");
+    int r = 0;
+    int c = 0;
+    while(true)
+    {
+        U64 bit_index = r * kRows + c;
+        U64 wall_bits = walls[bit_index / 64];
+        U64 mask_asdf = (63 - (bit_index % 64));
+        U64 mask = (1ULL << mask_asdf);
+        if(wall_bits != 0)
+        {
+            int asdf=0;
+            asdf++;
+        }
+        bool bit_set = wall_bits & mask;
+        if(r == kStartY && c == kStartX)
+        {
+            printf("O");
+        }
+        else if(r == kEndY && c == kEndX)
+        {
+            printf("O");
+        }
+        else if(bit_set)
+        {
+            printf(".");
+        }
+        else
+        {
+            printf(" ");
+        }
+
+        c++;
+        if(c >= kCols)
+        {
+            c = 0;
+            r++;
+            printf("\n");
+        }
+        if(r >= kRows)
+        {
+            break;
+        }
+    }
+}
+
+void verify_walls(const U64 *walls, double *g_scores)
+{
+    printf("\n\n\n");
+    for(int r = 0; r < kRows; r++)
+    {
+        for(int c = 0; c < kCols; c++)
+        {
+            U64 bit_index = r * kRows + c;
+            U64 wall_bits = walls[bit_index / 64];
+            U64 mask_asdf = (63 - (bit_index % 64));
+            U64 mask = (1ULL << mask_asdf);
+            bool bit_set = wall_bits & mask;
+            if(bit_set)
+            {
+                if(g_scores[bit_index] != INFINITY)
+                {
+                    printf("BADDDDDDDDDDDDDDDDDDD");
+                }
+            }
+        }
+    }
+}
+
+#endif
+
 /// your code goes here!
 float FastPathFind(const U64* pWalls)
 {
-    std::vector<U32> m_openSet;
-    m_openSet.reserve(1000);
-    std::unordered_set<U32> m_visited;
-    double m_f[kCells];
+    // while not at the end
+    //     add neighbors
+    //         guess distance to the end
+    //         if g score is greater than current, update it
+    //     move to the next cheapest node on the open list
 
-    for (U32 i = 0; i < kCells; ++i)
-        m_f[i] = 99999999999.0;
+    //print_walls(pWalls);
 
-    m_f[kStartI] = 0.0;
-    m_openSet.emplace_back(kStartI);
-
-    do
+    // Initialize grid
+    static double g_scores[kRows][kCols] = {};
+    for(int r = 0; r < kRows; r++)
     {
-        std::vector<U32>::iterator it = std::min_element(m_openSet.begin(), m_openSet.end(),
-            [&](const U32 a, const U32 b) { return m_f[a] < m_f[b]; });
-        const U32 eI = *it;
-        const double f = m_f[eI];
-        m_visited.emplace(eI);
-        m_openSet.erase(it);
-
-        if (eI == kEndI)
-            return float(f);
-
-        const U32 eX = eI % kCols;
-        const U32 eY = eI / kCols;
-
-        for (const std::pair<U32, U32>& p : std::initializer_list<std::pair<U32, U32>>{
-            { eX - 1, eY }, { eX + 1, eY }, { eX, eY - 1 }, { eX, eY + 1 },
-            { eX - 1, eY - 1}, { eX - 1, eY + 1 }, { eX + 1, eY - 1 }, { eX + 1, eY + 1 } })
+        for(int c = 0; c < kCols; c++)
         {
-            const U32 x = p.first;
-            const U32 y = p.second;
-            const U32 i = y * kCols + x;
-            if (x < kCols && y < kRows && m_visited.find(i) == m_visited.end() &&
-                !(pWalls[i >> 6] & (1ULL << (i & 63ULL))))
+            g_scores[r][c] = INFINITY;
+        }
+    }
+
+    v2 target = {kEndX, kEndY};
+    std::vector<Node> open_list = { {{kStartX, kStartY}, 0.0f } };
+    g_scores[kStartY][kStartX] = 0.0f;
+
+    while(!open_list.empty())
+    {
+        // Find the cheapest node on the open list
+        Node current = open_list[0];
+        for(const Node &node : open_list)
+        {
+            if(node.f_score < current.f_score)
             {
-                if (std::find(m_openSet.begin(), m_openSet.end(), i) == m_openSet.end())
-                    m_openSet.emplace_back(i);
-                m_f[i] = std::fmin(m_f[i], f + ((x == eX || y == eY) ? 1.0 : 1.4142135623730951));
+                current = node;
             }
         }
-    } while (!m_openSet.empty());
+        // Remove current from the open list
+        for(Node &node : open_list)
+        {
+            if(node.pos == current.pos)
+            {
+                std::swap(node, open_list.back());
+                break;
+            }
+        }
+        open_list.pop_back();
 
-    return -1.0f;
+        int num_neighbors = 0;
+        v2 neighbors[8];
+        double neighbor_distances[8];
+        find_neighbors(current.pos, pWalls, neighbors, neighbor_distances, &num_neighbors);
+        for(int n = 0; n < num_neighbors; n++)
+        {
+            v2 neighbor = neighbors[n];
+            double neighbor_distsance = neighbor_distances[n];
+
+            double neighbor_new_g_score = g_scores[current.pos.y][current.pos.x] + neighbor_distsance;
+
+            // Check if done
+            if(neighbor == target)
+            {
+                // Done
+                verify_walls(pWalls, g_scores[0]);
+                return neighbor_new_g_score;
+            }
+
+            if(neighbor_new_g_score < g_scores[neighbor.y][neighbor.x])
+            {
+                g_scores[neighbor.y][neighbor.x] = neighbor_new_g_score;
+                double f_score = neighbor_new_g_score + heuristic(neighbor, target);
+
+                bool found_on_open_list = false;
+                for(const Node &node : open_list)
+                {
+                    if(node.pos == neighbor)
+                    {
+                        found_on_open_list = true;
+                        break;
+                    }
+                }
+                if(!found_on_open_list)
+                {
+                    open_list.push_back({neighbor, f_score});
+                }
+            }
+        }
+    }
+
+    verify_walls(pWalls, g_scores[0]);
+    return -1.0;
 }
+
+
+
